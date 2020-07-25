8ece95ea2484cfd169b4f012d87493a21c29b855
21toffy <oketofoke@gmail.com>
2020-07-25 16:28:34 +0100

    nicer foooter

--- src/components/Body/Riskchecker.js
+++ src/components/Body/Riskchecker.js
@@ -10,11 +10,11 @@ export default function Riskchecker({ props }) {
     return(
         <div className="second-section">
 <div className="row">
-    <div className="contain col-md-8">
-    <img className="" src={"https://i.pinimg.com/originals/ed/1a/70/ed1a707b12d96edc3e390d4ecc419b3a.jpg" } alt={"https://www.researchgate.net/profile/Ezra_Gayawan/publication/260023562/figure/fig1/AS:297066292105225@1447837531741/Map-of-Nigeria-showing-the-36-states-and-Federal-Capital-Territory-FCT-Abuja.png"} />
+    <div className="contain col-sm-6 col-md-8 ">
+    <img className="w-70" src={"https://i.pinimg.com/originals/ed/1a/70/ed1a707b12d96edc3e390d4ecc419b3a.jpg" } alt={"https://www.researchgate.net/profile/Ezra_Gayawan/publication/260023562/figure/fig1/AS:297066292105225@1447837531741/Map-of-Nigeria-showing-the-36-states-and-Federal-Capital-Territory-FCT-Abuja.png"} />
     </div>
 
-    <div className="col-md-4">
+    <div className="col-sm-6 col-md-4 ">
     <div className="card" style={{width:"18rem"}}>
     <ul className="list-group list-group-flush">
         <li className="list-group-item">Risk Checker</li>
